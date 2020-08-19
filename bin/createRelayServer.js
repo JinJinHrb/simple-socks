2fbbfe618227534d6694d2fedf1674def02fc315
yamBean <fan.w_2005@163.com>
2020-08-19 17:52:22 +0800

    更新注释

--- bin/createRelayServer.js
+++ bin/createRelayServer.js
@@ -8,12 +8,15 @@
 	
 	(2) 创建 TCP 转发
 	// node "/Users/alexwang/Documents/workspace/github/simple-socks-copy/examples/forwarder.js" 1080 127.0.0.1:1081
-	// supervisor --inspect bin/createRelayServer.js
-	node bin/createRelayServer.js
+	// supervisor --inspect "/Users/alexwang/Documents/workspace/github/simple-socks-jinjinHrb/bin/createRelayServer.js"
+	node "/Users/alexwang/Documents/workspace/github/simple-socks-jinjinHrb/bin/createRelayServer.js"
 
 	(3) 测试网页
 	curl http://www.baidu.com --socks5 127.0.0.1:1080
 	curl http://info.so.com/feedback.html --socks5 127.0.0.1:1080
+
+	(4) 测试带用户名/密码服务器
+	curl -x socks5://ivisa.vip:F2e1AeC06f3993FB@localhost:1099/ "https://www.so.com/"
 
 */
 const Config = require('../config.js');
